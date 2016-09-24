06b6bc5cbf139bf2fa6042abb712642d0a6d815d
Sagar Chakravarthy <bp.sagar@gmail.com>
2016-09-24 10:13:40 +0530

    Adding gitignore, requirements and setup file.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='css2video',
+    version='1.0.0',
+    author='Sagar Chakravarthy',
+    author_email='bp.sagar@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ],
+    zip_safe=False
+)
